661773334e030421fb737d67ffc41c72739851b3
bryan <bryanmendez21@icloud.com>
2026-04-16 13:30:24 -0400

    Created a while loop to run I love java 5 times. created new class for  do while loop

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -3,5 +3,12 @@ package com.pluralsight;
 public class WhileLoop {
     public static void main(String[] args) {
 
+        int count = 0;
+
+        while (count < 5){
+            System.out.println("I Love Java");
+            count ++;
+        }
+
     }
 }
